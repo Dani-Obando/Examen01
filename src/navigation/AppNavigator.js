f8dc81ff6564c29dc643ef589838c30f0728d0c4
Dani-Obando <daobandona@est.utn.ac.cr>
2025-03-03 12:27:23 -0600

    arreglo de favoritos

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -4,14 +4,14 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeView from "../views/HomeView";
 import PlaceDetailView from "../views/PlaceDetailView";
-import FavoritesView from "../views/FavoritesView"
+import FavoritesView from "../views/FavoritesView";
 import { View } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-// Stack para la pantalla Home con detalles de lugares
+// Stack para la pantalla Home
 function HomeStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
@@ -21,35 +21,38 @@ function HomeStack() {
   );
 }
 
-// Componente principal con las Tabs
+// Stack para Favoritos
+function FavoritesStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Favorites" component={FavoritesView} />
+      <Stack.Screen name="PlaceDetail" component={PlaceDetailView} />
+    </Stack.Navigator>
+  );
+}
+
+// Navegación principal con Tabs e Íconos
 export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
           headerShown: false,
-          tabBarStyle: {
-            backgroundColor: "#fff",
-            height: 60,
-            paddingBottom: 5,
-          },
           tabBarIcon: ({ color, size }) => {
             let iconName;
-
-            if (route.name === "Explorar") {
-              iconName = "search-outline";
-            } else if (route.name === "Favoritos") {
+            if (route.name === "HomeTab") {
+              iconName = "home-outline";
+            } else if (route.name === "FavoritesTab") {
               iconName = "heart-outline";
             }
-
             return <Icon name={iconName} size={size} color={color} />;
           },
-          tabBarActiveTintColor: "#007AFF",
+          tabBarActiveTintColor: "tomato",
           tabBarInactiveTintColor: "gray",
         })}
       >
-        <Tab.Screen name="Explorar" component={HomeStack} />
-        <Tab.Screen name="Favoritos" component={FavoritesView} />
+        <Tab.Screen name="HomeTab" component={HomeStack} options={{ title: "Inicio" }} />
+        <Tab.Screen name="FavoritesTab" component={FavoritesStack} options={{ title: "Favoritos" }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
